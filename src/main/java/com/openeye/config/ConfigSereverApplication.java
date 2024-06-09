e1c77893b52baf2de2314a73c5b978cba831a3a4
dheeraj chaudhary <dheerajneerajshiv@gmail.com>
2024-06-09 21:43:16 +0530

    initial commit

---
+++ src/main/java/com/openeye/config/ConfigSereverApplication.java
@@ -0,0 +1,15 @@
+package com.openeye.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigSereverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigSereverApplication.class, args);
+	}
+
+}
